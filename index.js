7e674d3887bb872d4cc431f55453c73195120b4d
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-25 10:38:25 -0500

    Built inquier, and some basic questions for structure.

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// description, 
+// installation instructions, 
+// usage information, 
+// contribution guidelines, 
+// test instructions
+// git hub username -make this a hype link to git hub page
+// email
+// Table of contents takes to the corresponding section
+
+const questions = [
+        {
+            type: `input`,                                                              
+            message: `What is your full name?`,
+            name: `name`
+        },
+        {
+            type: `input`,                                                              
+            message: `What languages did you use?`,
+            name: `languages`
+        },
+        {
+            type: ``,                                                              
+            message: ``,
+            name: ``
+        },
+        {
+            type: ``,                                                              
+            message: ``,
+            name: ``
+        }
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt(questions)
+
+
+
+    .then((response) => {
+            //console.log(response.name, response.languages, response.comm)
+        fs.writeFile(`ReadMETemplate.JSON`, JSON.stringify(response, null),(err)=>
+        err ? console.log(err) : console.log(`success!`)
+        )
+    });
+
+JSON.parseInt();
 
 // TODO: Create a function to initialize app
 function init() {}
